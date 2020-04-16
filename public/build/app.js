4e3d9dae5b6f4ad8fa710444f7986b9071451dea
rundaer <rundaer@gmail.com>
2020-04-16 23:12:01 +0200

    configure symfony4 - webpack

---
+++ public/build/app.js
@@ -0,0 +1,29 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([["app"],{
+
+/***/ "./assets/css/app.css":
+/*!****************************!*\
+  !*** ./assets/css/app.css ***!
+  \****************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+// extracted by mini-css-extract-plugin
+
+/***/ }),
+
+/***/ "./assets/js/app.js":
+/*!**************************!*\
+  !*** ./assets/js/app.js ***!
+  \**************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+// assets/js/app.js
+__webpack_require__(/*! ../css/app.css */ "./assets/css/app.css");
+
+console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
+
+/***/ })
+
+},[["./assets/js/app.js","runtime"]]]);
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly8vLi9hc3NldHMvY3NzL2FwcC5jc3MiLCJ3ZWJwYWNrOi8vLy4vYXNzZXRzL2pzL2FwcC5qcyJdLCJuYW1lcyI6WyJyZXF1aXJlIiwiY29uc29sZSIsImxvZyJdLCJtYXBwaW5ncyI6Ijs7Ozs7Ozs7O0FBQUEsdUM7Ozs7Ozs7Ozs7O0FDQUE7QUFFQUEsbUJBQU8sQ0FBQyw0Q0FBRCxDQUFQOztBQUVBQyxPQUFPLENBQUNDLEdBQVIsQ0FBWSxtREFBWixFIiwiZmlsZSI6ImFwcC5qcyIsInNvdXJjZXNDb250ZW50IjpbIi8vIGV4dHJhY3RlZCBieSBtaW5pLWNzcy1leHRyYWN0LXBsdWdpbiIsIi8vIGFzc2V0cy9qcy9hcHAuanNcblxucmVxdWlyZSgnLi4vY3NzL2FwcC5jc3MnKTtcblxuY29uc29sZS5sb2coJ0hlbGxvIFdlYnBhY2sgRW5jb3JlISBFZGl0IG1lIGluIGFzc2V0cy9qcy9hcHAuanMnKTsiXSwic291cmNlUm9vdCI6IiJ9
